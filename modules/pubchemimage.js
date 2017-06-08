9ea9c9262fc1f1bb501fa18aca932b64359dbb77
Thomas Rory Gummerson <thomas@gummerson.no>
2017-06-08 22:18:48 +0200

    Removed jsdom dependency and optimized code in pubchemimage module

--- modules/pubchemimage.js
+++ modules/pubchemimage.js
@@ -1,24 +1,21 @@
 'use strict';
 
-const { JSDOM } = require('jsdom');
+const https = require('https');
+
+const get = url =>
+	new Promise((resolve, reject) =>
+		https.get(url, res => {
+			let data = '';
+			res
+				.on('error', reject)
+				.on('data', d => data += d)
+				.on('end', () => resolve(data));
+		}).on('error', reject));
 
 const compoundURL = 'https://pubchem.ncbi.nlm.nih.gov/compound/';
 const imageURL = 'https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?t=l&cid=';
 
-module.exports = compound => new Promise((resolve, reject) => {
-	JSDOM.fromURL(compoundURL + compound)
-		.then(dom => {
-			const metaElem = dom.window.document.body
-				.querySelector('meta[name="pubchem_uid_value"]');
-			if (metaElem) {
-				const cid = metaElem.getAttribute('content');
-				if (cid) {
-					resolve(imageURL + cid);
-				} else {
-					reject(Error('CID not found'));
-				}
-			} else {
-				reject(Error('Element not found'));
-			}
-		});
-});
+module.exports = compound => get(compoundURL + compound)
+	.then(body =>
+		body.match(/<meta name="pubchem_uid_value" content="(\d+)"/)[1])
+	.then(result => imageURL + result);
