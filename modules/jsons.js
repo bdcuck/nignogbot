4e8f5c0a851f05b52481cf3edbdcf49628bd69d4
Thomas Rory Gummerson <thomas@gummerson.no>
2017-06-30 23:04:09 +0200

    Json tidying

--- modules/jsons.js
+++ modules/jsons.js
@@ -1,12 +1,14 @@
+'use strict';
+
 module.exports = {
-    jokes: require('../json/jokes.json'),
-    help : require('../json/help.json').general.join(' '),
-    nigger : require('../json/nigger.json'),
-    actions : require('../json/actions.json'),
-    atom : require('../json/atom.json'),
-    hitler : require('../json/hitlerStickers.json'),
-    weeb : require('../json/weebStickers.json'),
-    nameFirst : require('../json/nameFirst.json'),
-    nameLast : require('../json/nameLast.json'),
-    fortune : require('../json/fortune.json')
+	actions: require('../json/actions.json'),
+	atom: require('../json/atom.json'),
+	fortune: require('../json/fortune.json'),
+	help: require('../json/help.json').general.join(' '),
+	hitler: require('../json/hitlerStickers.json'),
+	jokes: require('../json/jokes.json'),
+	nameFirst: require('../json/nameFirst.json'),
+	nameLast: require('../json/nameLast.json'),
+	nigger: require('../json/nigger.json'),
+	weeb: require('../json/weebStickers.json')
 };
