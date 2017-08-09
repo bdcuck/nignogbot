33e87b17539ab5ead2f19ab3d5465c7eb151a5e7
bdbdbd <info@medchem.nl>
2017-08-09 18:43:46 +0200

    fixed a fat derp in balance

--- commands/balance.js
+++ commands/balance.js
@@ -2,42 +2,23 @@
 
 const jsdom = require('jsdom/lib/old-api.js');
 
-const balance = (commandText) => {
-	jsdom.env('http://www.webqc.org/balance.php?reaction=' +
-				encodeURIComponent(commandText), (err, win) => {
-					if (err) return;
-                    let text = '';
-					const equation = win.document
-						.querySelector('td.center > b')
-						.textContent;
-					const reaction = win.document
-						.querySelector('td.center > br')
-						.nextSibling
-						.textContent;
-					text = equation + '\n' + reaction;
-                    console.log(text);
-                    return text;
-				});
-    
-};
-module.exports = ({ reply, message }) =>  {
+module.exports = ({ reply, message }) => {
     let formula = message.text.split(/\s+/);
-    let cmd = formula.shift();
-    formula = formula.join(' ');
-	jsdom.env('http://www.webqc.org/balance.php?reaction=' +
-				encodeURIComponent(formula), (err, win) => {
-					if (err) return;
-                    let text = '';
-					const equation = win.document
-						.querySelector('td.center > b')
-						.textContent;
-					const reaction = win.document
-						.querySelector('td.center > br')
-						.nextSibling
-						.textContent;
-					text = equation + '\n' + reaction;
-                    console.log(text);
-                    return reply(text);
-				});
-    
+    formula = formula.splice(1, formula.length).join(' ');
+    jsdom.env('http://www.webqc.org/balance.php?reaction=' +
+        encodeURIComponent(formula), (err, win) => {
+            if (err) return;
+            let text = '';
+            const equation = win.document
+                .querySelector('td.center > b')
+                .textContent;
+            const reaction = win.document
+                .querySelector('td.center > br')
+                .nextSibling
+                .textContent;
+            text = equation + '\n' + reaction;
+            console.log(text);
+            return reply(text);
+        });
+
 };
