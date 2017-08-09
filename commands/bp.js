cea175907e6e413f743571bac8a1885f88965aaa
bdbdbd <info@medchem.nl>
2017-08-09 21:39:58 +0200

    boiling and melting point shit

--- commands/bp.js
+++ commands/bp.js
@@ -1,7 +1,28 @@
 'use strict';
 
-const bp = (commandText) => { return 'feature currently under construction faggot' 
+const pt = require('periodic-table');
+const util = require('periodic-table/util');
 
-    
-};
-module.exports = ({ reply }) => reply(bp());
+const capitalizeFirstLetter = str => str[0].toUpperCase() + str.slice(1);
+
+module.exports = ({ reply, message }) => {
+    let formula = message.text.split(/\s+/);
+    formula = formula.splice(1, formula.length).join(' ');
+    formula ? formula = capitalizeFirstLetter(formula) : formula = ' ';
+
+    if (pt.symbols[formula]) {
+        const celsius =
+            Number(pt.symbols[formula].boilingPoint) - 273;
+        return reply('The boiling point of ' + formula + ' is ' +
+            pt.symbols[formula].boilingPoint + ' K or ' +
+            celsius + 'ºC');
+    } else if (pt.elements[formula]) {
+        const celsius =
+            Number(pt.elements[formula].boilingPoint) - 273;
+        return reply('The boiling point of ' + formula + ' is ' +
+            pt.elements[formula].boilingPoint + ' K or ' +
+            celsius + 'ºC');
+    } else {
+        return reply('Not found!');
+    }
+}
