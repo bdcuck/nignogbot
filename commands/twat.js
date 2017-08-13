bcfab77c525b75f0c59471d6a109cdd00c6814e1
bdbdbd <info@medchem.nl>
2017-08-13 18:59:09 +0200

    fixed the twatter

--- commands/twat.js
+++ commands/twat.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const twat = (commandText) => { return 'feature currently under construction faggot' 
+const { commandText } = require('../utils');
+const config = require('../config.json');
+const TwitterPackage = require('twitter');
+const Twitter = new TwitterPackage(config.twitter);
 
-    
-};
-module.exports = ({ reply }) => reply(twat());
+module.exports = ({ reply, message }) => {
+    if (commandText(message)) {
+        const tweet = commandText(message) + ' - ' + message.from.first_name;
+        if (140 >= tweet.length)
+            Twitter.post('statuses/update', {
+                status: tweet
+            }, (err) => {
+                if (err)
+                    return reply(err);
+                reply(
+                    'Messaged tweeted https://twitter.com/rambodildo');
+            });
+        else
+            reply('Too long, you boner!');
+    } else {
+        reply('You forgot your message retard');
+    }
+}
