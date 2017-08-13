b8c6a604e5ca4cb7b7ce45e3478a60cff6d4d015
bdbdbd <info@medchem.nl>
2017-08-13 22:06:47 +0200

    sponge added

--- commands/sponge.js
+++ commands/sponge.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const sponge = (commandText) => { return 'feature currently under construction faggot' 
+const { commandText, caps } = require('../utils');
 
-    
-};
-module.exports = ({ reply }) => reply(sponge());
+module.exports = ({ replyWithPhoto, message }) => {
+    let spongemock = commandText(message);
+    if (!spongemock) spongemock =
+        'I\'m too retarded to type some text';
+    spongemock = caps(spongemock);
+    replyWithPhoto('http://i1.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg', {caption: spongemock});
+}
