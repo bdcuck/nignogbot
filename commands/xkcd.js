3feacdd23eedd1d2bea0ff04f675638a69c3f3bc
Thomas Rory Gummerson <thomas@gummerson.no>
2017-08-16 11:09:01 +0200

    Shortened xkcd

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -1,25 +1,14 @@
 'use strict'
 
 const { json } = require('req');
-const randComic = max => {
-	let num;
-	do {
-	  num = Math.floor((Math.random() * max) + 1);
-	} while (num == 404);
-	return num;
-  }
 
-module.exports = ({ replyWithPhoto }) => {
-	let counter = 1;
+const rand = max => (num => num === 404
+	? rand(max)
+	: num)(Math.floor((Math.random() * max) + 1));
+
+module.exports = ({ replyWithPhoto }) =>
 	json('https://xkcd.com/info.0.json')
-	.then(data => counter = data.num)
-	.then(() => {
-		counter = randComic(counter);
-		json(`https://xkcd.com/${counter}/info.0.json`)
-		.then(res => {
-			replyWithPhoto(res.img, { caption: res.title });
-		})
-
-	})
-
-}
+		.then(data =>
+			json(`https://xkcd.com/${rand(data.num)}/info.0.json`))
+		.then(data =>
+			replyWithPhoto(data.img, { caption: data.title }));
