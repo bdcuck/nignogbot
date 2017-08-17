98454467aa9b0b5b8bc875a6038a84491b7e862f
bdbdbd <info@medchem.nl>
2017-08-17 18:50:18 +0200

    dilbert draft

---
+++ commands/dilbert.js
@@ -0,0 +1,27 @@
+'use strict'
+/*
+const { request } = require('request');
+
+const yyyymmdd = date => {
+	return {
+		year: date.getFullYear(),
+		month: date.getMonth() + 1,
+		day: date.getDate()
+	}
+}
+
+const formatDate = date => date.year + '-' + date.month + '-' + date.day;
+
+const randomDate = (start, end) => new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+
+const firstDilbert = new Date(1989, 03, 17);
+
+let yesterday = new Date()
+yesterday.setDate(yesterday.getDate() - 1)
+let date = yyyymmdd(randomDate(firstDilbert, yesterday))
+let formatted = formatDate(date)
+// request shit to figure out
+request('http://www.dilbert.com/strip/'+formatted, blabla) 
+
+module.exports = getDilbert
+*/
