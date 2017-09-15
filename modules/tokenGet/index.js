649ed74a37761edf3b33d9619eff45723d32aa30
Thomas Rory Gummerson <thomas@gummerson.no>
2017-09-15 21:52:21 +0200

    Handle all rejections

--- modules/tokenGet/index.js
+++ modules/tokenGet/index.js
@@ -7,14 +7,14 @@ const regex = /\d{9}:.{35}/g;
 const checker = ({ message, reply }) =>
 	Promise.all(message.text.match(regex).map(token =>
 		json('https://api.telegram.org/bot' + token + '/getMe')))
-		.then(results => results
+		.then(results => Promise.all(results
 			.filter(bot => bot.ok)
 			.map(bot =>
 				'Valid token detected!' +
 				'\nid: ' + bot.result.id +
 				'\nfirst_name: ' + bot.result.first_name +
 				'\nusername :' + bot.result.username)
-			.map(reply));
+			.map(reply)));
 
 module.exports = [
 	regex,
