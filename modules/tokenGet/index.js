ceb848eb042d95eb56fab9c9a5f58f9cd3fee90b
Thomas Rory Gummerson <thomas@gummerson.no>
2017-09-15 21:44:09 +0200

    Tiny fix

--- modules/tokenGet/index.js
+++ modules/tokenGet/index.js
@@ -4,9 +4,8 @@ const { json } = require('req');
 
 const regex = /\d{9}:.{35}/g;
 
-const checker = ({ message, reply }) => {
-	const tokens = message.text.match(regex);
-	Promise.all(tokens.map(token =>
+const checker = ({ message, reply }) =>
+	Promise.all(message.text.match(regex).map(token =>
 		json('https://api.telegram.org/bot' + token + '/getMe')))
 		.then(results => results
 			.filter(bot => bot.ok)
@@ -15,11 +14,9 @@ const checker = ({ message, reply }) => {
 				'\nid: ' + bot.result.id +
 				'\nfirst_name: ' + bot.result.first_name +
 				'\nusername :' + bot.result.username)
-			.forEach(reply));
-};
+			.map(reply));
 
 module.exports = [
 	regex,
 	checker
 ];
-
