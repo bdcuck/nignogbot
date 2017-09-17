bfbe5ed05eaf17b6d67eae137175cf5fe546a0ff
bdbdbd <info@medchem.nl>
2017-09-17 18:45:40 +0200

    add polakname

--- commands/gayname.js
+++ commands/gayname.js
@@ -2,4 +2,4 @@
 const { nameFirst, nameLast } = require('../modules/jsons.js');
 const rand = arr => arr[Math.floor(Math.random() * arr.length)];
 const getRandomName = () => rand(nameFirst) + ' ' + rand(nameLast);
-module.exports = ({ from, reply }) => reply(from.first_name + ' aka ' + getRandomName());
+module.exports = ({ from, reply }) => from.language_code === 'pl-PL' ? reply(from.first_name + ' aka FILTHY PIECE OF RETARD POLAK MIDGETSHIT') : reply(from.first_name + ' aka ' + getRandomName());
