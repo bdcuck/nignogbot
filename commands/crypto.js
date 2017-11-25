09cf941d98e5809590c6a2ca6ca742f7ef2c3cc6
bdbdbd <info@medchem.nl>
2017-11-25 14:49:58 +0100

    bonner

--- commands/crypto.js
+++ commands/crypto.js
@@ -6,7 +6,7 @@ let shit;
 
 module.exports = ({ reply, message }) => {
 	let res = '';
-	json('https://api.coinmarketcap.com/v1/ticker/')
+	json('https://api.coinmarketcap.com/v1/ticker/?limit=0')
 		.then(data => shit = data).then(() => {
 			const tick = shit.find(x => x.symbol === commandText(message).toUpperCase());
 			if (tick === undefined) return reply('Input a valid ticker symbol dumbass.');
