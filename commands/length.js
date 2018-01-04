ef0437227919286dcdc3c862736a84af22cf508f
bdbdbd <info@medchem.nl>
2018-01-04 16:02:46 +0100

    forgot dumb niggerspacer

--- commands/length.js
+++ commands/length.js
@@ -4,7 +4,7 @@ const { commandText } = require('../utils');
 
 module.exports = ({ reply, message }) => {
 	if (commandText(message)){ 
-        const msgLength = (commandText(message) + message.from.first_name).length - 280
+        const msgLength = (commandText(message) + message.from.first_name + ' - ').length - 280
         return reply(`
         Message length: ${commandText(message).length}
         Twat characters ${msgLength < 0 ? 'remaining: ' + Math.abs(msgLength) : 'exceeded by: ' + Math.abs(msgLength)}`);
