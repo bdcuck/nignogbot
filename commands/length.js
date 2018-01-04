427b994f750217e83022e46598d3bb27fd1a4236
bdbdbd <info@medchem.nl>
2018-01-04 15:59:59 +0100

    piss nigger clit

--- commands/length.js
+++ commands/length.js
@@ -4,9 +4,10 @@ const { commandText } = require('../utils');
 
 module.exports = ({ reply, message }) => {
 	if (commandText(message)){ 
+        const msgLength = (commandText(message) + message.from.first_name).length - 280
         return reply(`
         Message length: ${commandText(message).length}
-        Twat characters remaining: ${commandText(message).length - 280}`);
+        Twat characters ${msgLength < 0 ? 'remaining: ' + Math.abs(msgLength) : 'exceeded by: ' + Math.abs(msgLength)}`);
     }
     return reply(`You forgot your message you bunghole.`);
 };
