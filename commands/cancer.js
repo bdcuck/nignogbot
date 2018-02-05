f7ab79a13325a9ee4e1c0486e63ec6ff11beaec5
bdbdbd <info@medchem.nl>
2018-02-05 21:13:33 +0100

    aa

--- commands/cancer.js
+++ commands/cancer.js
@@ -3,7 +3,7 @@
 const { commandText, niggerify } = require('../utils');
 
 module.exports = ({ message, reply }) => {
-    const msg = message.reply_to_message ? commandText(message.reply_to_message) : commandText(message);
+    const msg = message.reply_to_message ? message.reply_to_message.text : commandText(message);
     if(!msg) return reply('🅿️🅾️🅾️🅿️🤪🆑ℹ️✝️');
     return reply(niggerify(msg, 4096));
 }
