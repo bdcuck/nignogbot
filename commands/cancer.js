cf029b017c4f98feede7aa4ac6c4896bee959e0d
bdbdbd <info@medchem.nl>
2018-02-05 20:45:38 +0100

    🅿️🅾️🅾️🅿️🤪🆑ℹ️✝️

---
+++ commands/cancer.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { commandText, niggerify } = require('../utils');
+
+module.exports = ({ message, reply }) => {
+    if(!commandText(message)) return reply('🅿️🅾️🅾️🅿️🤪🆑ℹ️✝️');
+    return reply(niggerify(commandText(message)));
+}
