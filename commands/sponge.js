0906a2255f20587357272a6c4869692b104232f6
bdbdbd <info@medchem.nl>
2018-02-16 22:16:45 +0100

    derp

--- commands/sponge.js
+++ commands/sponge.js
@@ -4,8 +4,9 @@ const { commandText, caps } = require('../utils');
 
 module.exports = ({ replyWithPhoto, message }) => {
 	let spongemock = message.reply_to_message ? message.reply_to_message.text : commandText(message);
+	let id = message.reply_to_message ? message.reply_to_message.message_id : message.message_id;
 	if (!spongemock) spongemock =
 								'I\'m too retarded to type some text';
 	spongemock = caps(spongemock);
-	replyWithPhoto('http://i1.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg', { caption: spongemock, reply_to_message_id: message.message_id });
+	replyWithPhoto('http://i1.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg', { caption: spongemock, reply_to_message_id: id });
 };
