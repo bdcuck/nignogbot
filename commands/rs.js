15a5c98fd3427d109272ff63a350e9a1ba31f09f
bdbdbd <info@medchem.nl>
2018-02-24 21:30:35 +0100

    rs stats kek

---
+++ commands/rs.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { commandText, capitalizeFirstLetter } = require('../utils');
+const rsapi = require('rs-api');
+
+module.exports = ({ replyWithMarkdown, message }) => {
+
+    const player = commandText(message);
+    if(!player) return replyWithMarkdown('You forgot to enter a username you *RETARD*');
+    rsapi.rs.player.hiscores(player)
+        .then(stats => {
+            let text = `Username: _${stats.username}_\n\n`;
+            let statArr = [];
+            for (let skl in stats.skills) {
+                statArr.push([skl, stats.skills[skl]]);
+            }   
+            statArr.sort((a,b) =>  b[1].exp - a[1].exp);    
+            statArr.forEach(x => text += `*${capitalizeFirstLetter(x[0])}*\n  Level ${x[1].level}, XP: ${x[1].exp}\n`)
+            replyWithMarkdown(text)
+        })
+        .catch(err => replyWithMarkdown(`Username _${player}_ not found hombre`));
+
+};
+
+module.exports.help = 'Runescape stats. Usage /rs [username]';
